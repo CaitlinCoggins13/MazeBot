c8dbd2b66d3d5b4bfac68d7d5d9d481f841ab912
Caitlin  Coggins <caitlincoggins@n236-89.mtholyoke.edu>
2017-04-18 17:09:50 -0400

    add senseUltrasonic.java

---
+++ src/SenseUltrasonic.java
@@ -0,0 +1,43 @@
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.navigation.ArcMoveController;
+import lejos.robotics.subsumption.Behavior;
+
+public class SenseUltrasonic implements Behavior {
+	private ArcMoveController robot;
+	private UltrasonicSensor sensor;
+	private boolean suppressed = false;
+
+	// array passed in here
+	public SenseUltrasonic(ArcMoveController r, UltrasonicSensor s) {
+		robot = r;
+		sensor = s;
+	}
+
+	@Override
+	public boolean takeControl() {
+		/**
+		 * if(!robot.isMoving()||!suppressed) { suppressed = false; return true;
+		 * } return false;
+		 **/
+		// if current cell is unvisited, suppressed = false, return true
+		// TODO:
+		return false;
+	}
+
+	@Override
+	public void action() {
+		// cm??
+		if (suppressed == false) {
+			if (sensor.getDistance() < 10) {
+				// update map to show wall
+			}
+		}
+
+	}
+
+	@Override
+	public void suppress() {
+		suppressed = true;
+	}
+
+}
